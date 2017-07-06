5432649f1e9c6e3c6cea25282e39963f72e8931a
deadlycrawler <deadlycrawler2@gmail.com>
2017-07-06 09:37:44 +0000

    I did it! updated to pull from network for data with minimal error, no fix for a URL redirect had to hard code a new URL

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -3,6 +3,7 @@ package com.example.android.quakereport;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -18,28 +19,29 @@ import java.util.ArrayList;
 
 public class EarthquakeActivity extends AppCompatActivity {
 
-    private static final String USGS_REQUEST_URL = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
-
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
+    private static final String USGS_REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=4&limit=20";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        // Create a fake list of earthquake locations.
-        //public Earthquake(double magnitude, String location, String date)
+        EarthquakeAsync task = new EarthquakeAsync();
+        task.execute(USGS_REQUEST_URL);
+
+    }
+
+    // Create a fake list of earthquake locations.
+    //public Earthquake(double magnitude, String location, String date)
 
 
-        final ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
-
+    private void updateUI(final ArrayList<Earthquake> earthquakes) {
 
         // Find a reference to the {@link ListView} in the layout
         EarthquakeAdapter adapter = new EarthquakeAdapter(this, earthquakes);
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
         earthquakeListView.setAdapter(adapter);
-
-
 
 
         earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -51,9 +53,23 @@ public class EarthquakeActivity extends AppCompatActivity {
                 Intent i = new Intent(Intent.ACTION_VIEW);
                 i.setData(Uri.parse(url));
                 startActivity(i);
-
-
             }
         });
     }
+
+
+    private class EarthquakeAsync extends AsyncTask<String, Void, ArrayList<Earthquake>> {
+        @Override
+        protected ArrayList<Earthquake> doInBackground(String... urls) {
+
+            final ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes(USGS_REQUEST_URL);
+            return earthquakes;
+        }
+
+        protected void onPostExecute(ArrayList<Earthquake> earthquakes) {
+            updateUI(earthquakes);
+        }
+
+    }
 }
+
